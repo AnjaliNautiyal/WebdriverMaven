42b7343379d068953c05485bb316f272f1b70063
Anjali Nautiyal <Anjali.Nautiyal@rsystems.com>
2017-10-16 15:16:13 +0530

    Initial Commit

---
+++ src/test/java/com/flipkart/configuration/Config.java
@@ -0,0 +1,24 @@
+package com.flipkart.configuration;
+
+public class Config {
+	
+	public static String Testsite ="https://www.flipkart.com/";
+	
+	
+	//Login Module
+	
+	public static final String Login = "html/body/div[1]/div/header/div[1]/div[1]/div/ul/li[9]/a";
+	public static final String Username = "html/body/div[2]/div/div/div/div/div[2]/div/form/div[1]/input";
+	public static final String Password = "html/body/div[2]/div/div/div/div/div[2]/div/form/div[2]/input";
+	public static final String LoginBttn  = "html/body/div[2]/div/div/div/div/div[2]/div/form/div[3]/button";
+	
+	//Women's Section
+	public static final String Womensection = "html/body/div[1]/div/header/div[2]/div/ul/li[4]/a/span";
+	public static final String Heels = "html/body/div[1]/div/header/div[2]/div/ul/li[4]/ul/li/ul/li[3]/ul/li[4]/a/span";
+	
+	//Men's Section
+	public static final String Mensection = "html/body/div[1]/div/header/div[2]/div/ul/li[3]/a/span";
+	public static final String Kurta = "html/body/div[1]/div/header/div[2]/div/ul/li[3]/ul/li/ul/li[2]/ul/li[4]/a/span";
+	
+
+}
---
+++ src/test/java/com/flipkart/pages/Home.java
@@ -0,0 +1,21 @@
+package com.flipkart.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.flipkart.configuration.Config;
+
+public class Home {
+	
+	@FindBy(xpath=Config.Login)
+	public WebElement Login;
+	
+	@FindBy(xpath=Config.Username)
+	public WebElement Username;
+	
+	@FindBy
+	
+	
+	
+
+}
